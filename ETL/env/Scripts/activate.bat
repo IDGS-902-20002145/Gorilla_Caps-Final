1471e8509a587a7c2c2c555b354dca3219c9b074
IDGS-902-20002145 <20002145@alumnos.utleon.edu.mx>
2023-11-26 19:59:16 -0600

    Commit Python y PWA
    Ya regreso a ser funcional el dashboard y se configuro los requerimientos para ser una PWA

--- ETL/env/Scripts/activate.bat
+++ ETL/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\zende\Documents\Proyecto Version Yahir\Gorilla_Caps-Yahir\ETL\env
+set VIRTUAL_ENV=C:\Users\zende\Documents\Proyecto Version Yahir\Gorilla_Caps\ETL\env
 
 if not defined PROMPT set PROMPT=$P$G
 
---
+++ PWA-Gorilla_Caps/src/components/Menu.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from "react";
+
+const Menu = () => {
+  return <h1>Menú</h1>;
+};
+
+export default Menu;
