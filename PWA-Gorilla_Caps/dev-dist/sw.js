1c508f402fb1bc69f8d49ab2638b978bd065c943
IDGS-902-20002180 <122686156+IDGS-902-20002180@users.noreply.github.com>
2023-12-05 13:18:33 -0600

    mnmn
    nju

--- PWA-Gorilla_Caps/dev-dist/sw.js
+++ PWA-Gorilla_Caps/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.adb6am0lrv8"
+    "revision": "0.72bmim87dno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
